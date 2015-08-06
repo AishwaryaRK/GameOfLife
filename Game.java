7c965dfb24caac10737b02248642f6870214ce55
AishwaryaRK <aishwarya.kaneri@gmail.com>
2015-08-06 18:39:34 +0530

    constants created

--- Game.java
+++ Game.java
@@ -5,10 +5,11 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public class Game {
+	private static final int SCORE_THREEE = 3;
+	private static final int SCORE_FOUR = 4;
 	private List<Cell> liveCells;
 	List<Cell> nextGeneration;
 
@@ -22,7 +23,7 @@ public class Game {
 			BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName));
 			String line = bufferedReader.readLine();
 			while (line != null) {
-				String[] s = line.split(",");
+				String[] s = line.split(Messages.getString("Game.0")); //$NON-NLS-1$
 				int x = Integer.parseInt(s[0]);
 				int y = Integer.parseInt(s[1]);
 				this.liveCells.add(new Cell(x, y));
@@ -55,10 +56,10 @@ public class Game {
 		}
 		int score = getScore(cell);
 		switch (score) {
-		case 3:
+		case SCORE_THREEE:
 			nextGeneration.add(cell);
 			break;
-		case 4:
+		case SCORE_FOUR:
 			if (isLive(cell)) {
 				nextGeneration.add(cell);
 			}
