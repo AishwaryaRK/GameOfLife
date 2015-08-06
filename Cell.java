b7384ac403eb43b7d27eb18768d099482ee4f42c
AishwaryaRK <aishwarya.kaneri@gmail.com>
2015-08-06 17:40:15 +0530

    equals overriden in cell class

--- Cell.java
+++ Cell.java
@@ -18,6 +18,20 @@ public class Cell {
 	}
 
 	@Override
+	public boolean equals(Object obj) {
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cell other = (Cell) obj;
+		if (x != other.x)
+			return false;
+		if (y != other.y)
+			return false;
+		return true;
+	}
+
+	@Override
 	public String toString() {
 		return "Cell [x=" + x + ", y=" + y + "]";
 	}
